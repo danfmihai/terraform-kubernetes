a26bbcc0528c05052dc8140020794abd85ef1178
root <danfmihai@hotmail.com>
2021-09-15 18:08:43 -0400

    added output

---
+++ output.tf
@@ -0,0 +1,17 @@
+output "master_id" {
+  value       = proxmox_vm_qemu.k8_master.*.id
+  description = "The id of the master."
+}
+
+output "master_ip" {
+    value = "${var.ip_master}${var.count_master}" 
+} 
+
+output "node_id" {
+  value       = proxmox_vm_qemu.k8_node.*.id
+  description = "The id of the master."
+}
+
+output "node_ip" {
+    value = proxmox_vm_qemu.k8_node[*].ipconfig0
+} 
