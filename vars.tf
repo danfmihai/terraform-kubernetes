c4477865b3d1e2c2a999c029954dd05fcc57dac6
root <danfmihai@hotmail.com>
2021-09-15 17:25:46 -0400

    edited

---
+++ vars.tf
@@ -0,0 +1,13 @@
+variable "pm_api_url" {}
+variable "token_id" {}
+variable "token_secret" {}
+variable "ssh_key1" {}
+
+variable "img_type" {}
+variable "cores" {}
+variable "memory" {}
+variable "ip" {}
+variable "startip" {}
+variable "gw" {}
+variable "count_vm" {}
+variable "proxmox_host" {}
